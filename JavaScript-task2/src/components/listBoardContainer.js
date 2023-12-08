749233d2a6ccdfe9873a64ea186362dc5a8080bc
Nerastr <106918395+Nerastr@users.noreply.github.com>
2023-12-08 18:59:29 +0300

    Delete JavaScript-task2 directory

--- JavaScript-task2/src/components/listBoardContainer.js
+++
@@ -1,7 +0,0 @@
-import { AbstractComponent } from "../abstractComponent.js";
-
-export class ListBoardComponent extends AbstractComponent {
-  getTemplate() {
-    return `<section class="board-app__list"></section>`;
-  }
-}
